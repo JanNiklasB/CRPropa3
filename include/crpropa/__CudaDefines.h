d06fca846a812f097ef8047347e0f0e7d5331411
Jan-Niklas Bohnensack <Jan-Niklas.Bohnensack@rub.de>
2025-10-17 13:24:24 +0200

    added error check macro for gpu calls

--- include/crpropa/__CudaDefines.h
+++ include/crpropa/__CudaDefines.h
@@ -6,6 +6,16 @@
 #define CUDA_CONSTANT __constant__
 #include <cuda_runtime.h>
 #include <cuda.h>
+#include <iostream>
+
+// see answer of talonmies on https://stackoverflow.com/a/14038590/30769038
+inline void gpuAssert(cudaError_t code, char * file, int line, bool Abort=true){
+	if (code != 0) {
+		fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code),file,line);
+		if (Abort) exit(code);
+	}
+}
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 
 #else
 #define CUDA_CALLABLE_MEMBER
