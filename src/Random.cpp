f94a0abb5af9f604261896a628d5b8993b66335a
Jan-Niklas Bohnensack <jan-niklas.bohnensack@rub.de>
2026-03-31 19:24:43 +0200

    fixed deprecated enum-enum-conversion

--- src/Random.cpp
+++ src/Random.cpp
@@ -390,11 +390,11 @@ void Random::initialize(const uint32_t seed) {
 void Random::reload() {
 	uint32_t *p = state;
 	int i;
-	for (i = N - M; i--; ++p)
+	for (i = +N - M; i--; ++p)
 		*p = twist(p[M], p[0], p[1]);
 	for (i = M; --i; ++p)
-		*p = twist(p[M - N], p[0], p[1]);
-	*p = twist(p[M - N], p[0], state[0]);
+		*p = twist(p[+M - N], p[0], p[1]);
+	*p = twist(p[+M - N], p[0], state[0]);
 
 	left = N, pNext = state;
 }
