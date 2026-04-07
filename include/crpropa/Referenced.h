a0c3da6a7ed1304f3986c265e024262a19b9de81
Jan-Niklas Bohnensack <jan-niklas.bohnensack@rub.de>
2026-04-07 10:56:35 +0200

    removed _ptr from ref_ptr ref_ptr now acts like a normal smart pointer and is able to hold a reference by using ref_ptr(T& obj)

--- include/crpropa/Referenced.h
+++ include/crpropa/Referenced.h
@@ -19,26 +19,24 @@ class ref_ptr {
 	public:
 	typedef T element_type;
 
-	ref_ptr() : _ptr(0), _shared_ptr(0) {}
-	ref_ptr(T* ptr) : _ptr(ptr), _shared_ptr(0), _is_shared(false) {}
+	ref_ptr() : _shared_ptr(0) {}
+	ref_ptr(T& obj) {
+		_shared_ptr = std::make_shared<T>(obj);
+	}
+	ref_ptr(T* ptr) {
+		_shared_ptr = std::shared_ptr<T>(ptr);
+	}
 	ref_ptr(const ref_ptr& rp) {
 		_shared_ptr = rp._shared_ptr;
-		_is_shared = rp._is_shared;
-		_ptr = rp._ptr;
 	}
 	template<class Other> ref_ptr(const ref_ptr<Other>& rp) {
 		_shared_ptr = rp._shared_ptr;
-		_is_shared = rp._is_shared;
-		_ptr = rp._ptr;
 	}
 	template<class Other> ref_ptr(const std::shared_ptr<Other>& shared_ptr) {
 		_shared_ptr = shared_ptr;
-		_is_shared = true;
-		_ptr = 0;
 	}
 
 	~ref_ptr() {
-		_ptr = 0;
 		_shared_ptr = 0;
 	}
 
@@ -52,10 +50,8 @@ class ref_ptr {
 		return *this;
 	}
 
-
 	inline ref_ptr& operator =(long int ptr) {
 		_shared_ptr = ptr;
-		_ptr = ptr;
 		return *this;
 	}
 
@@ -64,16 +60,10 @@ class ref_ptr {
 	// }
 
 	T& operator*() const {
-		if (_is_shared)
-			return *_shared_ptr;
-		else
-			return *_ptr;
+		return *_shared_ptr;
 	}
 	T* operator->() const {
-		if (_is_shared)
-			return _shared_ptr.get();
-		else
-			return _ptr;
+		return _shared_ptr.get();
 	}
 
 	bool operator==(const ref_ptr& rp) const {
@@ -81,60 +71,30 @@ class ref_ptr {
 	}
 
 	T* get() const {
-		if (_is_shared)
-			return _shared_ptr.get();
-		else
-			return _ptr;
+		return _shared_ptr.get();
 	}
 
-	/** Returns stored shared_ptr
-	 * In case of a stored normal pointer this only return NULL;
-	 * User need to check if returned pointer is valid
-	 */
 	std::shared_ptr<T> get_shared() const {
 		return _shared_ptr;
 	}
 
 	bool valid() const {
-		if (_is_shared)
-			return _shared_ptr != 0;
-		else
-			return _ptr != 0;
+		return _shared_ptr != 0;
 	}
 
-	/** swaps two reference pointer
-	 * Returns zero if unsuccessfull
-	 */
-	int swap(ref_ptr& rp) {
-		if(rp._is_shared!=_is_shared)
-			return 0;
-		if(_is_shared){
-			_shared_ptr.swap(rp._shared_ptr);
-		} else {
-			T* tmp = _ptr;
-			_ptr = rp._ptr;
-			rp._ptr = tmp;
-		}
-		return 1;
-	}
-
-	bool is_shared() const{
-		return _is_shared;
+	void swap(ref_ptr& rp) {
+		_shared_ptr.swap(rp._shared_ptr);
 	}
 
 	private:
 
 	template<class Other> void assign(const ref_ptr<Other>& rp) {
 		_shared_ptr = rp._shared_ptr;
-		_is_shared = rp._is_shared;
-		_ptr = rp._ptr;
 	}
 
 	template<class Other> friend class ref_ptr;
 
-	T* _ptr = 0;
 	std::shared_ptr<T> _shared_ptr = 0;
-	bool _is_shared=false;
 };
 
 template<class T> inline
@@ -144,26 +104,17 @@ void swap(ref_ptr<T>& rp1, ref_ptr<T>& rp2) {
 
 template<class T, class Y> 
 inline ref_ptr<T> static_pointer_cast(const ref_ptr<Y>& rp) {
-	if (rp.is_shared())
-		return std::static_pointer_cast<T>(rp.get_shared());
-	else
-		return static_cast<T*>(rp.get());
+	return std::static_pointer_cast<T>(rp.get_shared());
 }
 
 template<class T, class Y> 
 inline ref_ptr<T> dynamic_pointer_cast(const ref_ptr<Y>& rp) {
-	if (rp.is_shared())
-		return std::dynamic_pointer_cast<T>(rp.get_shared());
-	else
-	return dynamic_cast<T*>(rp.get());
+	return std::dynamic_pointer_cast<T>(rp.get_shared());
 }
 
 template<class T, class Y> 
 inline ref_ptr<T> const_pointer_cast(const ref_ptr<Y>& rp) {
-	if (rp.is_shared())
-		return std::const_pointer_cast<T>(rp);
-	else
-	return const_cast<T*>(rp.get());
+	return std::const_pointer_cast<T>(rp);
 }
 
 /** @}*/
