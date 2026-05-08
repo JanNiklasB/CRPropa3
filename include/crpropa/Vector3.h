fb9c12d28a28abff0320561b87c81d05155066c0
Jan-Niklas Bohnensack <jan-niklas.bohnensack@rub.de>
2026-05-08 02:07:36 +0200

    reverted back to master Vector3.h

--- include/crpropa/Vector3.h
+++ include/crpropa/Vector3.h
@@ -6,13 +6,13 @@
 #include <vector>
 #include <limits>
 #include <algorithm>
-#include <unistd.h>
-
-#if defined(Python_FOUND) or defined(SWIG)
+#ifdef CRPROPA_HAVE_PYTHON
 #include <Python.h>
 #include <numpy/arrayobject.h>
+#endif // CRPROPA_HAVE_PYTHON
+#include <unistd.h>
+
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION 
-#endif
 
 namespace crpropa {
 
@@ -424,12 +424,10 @@ public:
 		return buffer;
 	}
 
-
-	#if defined(Python_FOUND) or defined(SWIG)
 	// ----------------------------
 	// 	Python numpy interface 
 	// ----------------------------
-
+#ifdef CRPROPA_HAVE_PYTHON
 	PyObject* __array__() {
 		npy_intp dims[1] = {3};
 		PyObject *array;
@@ -496,8 +494,8 @@ public:
 		// free the reference to the numpy array
 		Py_DECREF(array);
 	}
-	#endif
-	
+#endif // CRPROPA_HAVE_PYTHON	
+
 };
 
 #ifndef SWIG
