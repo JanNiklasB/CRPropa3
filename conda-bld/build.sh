87cb0a02891582d2b90ad11f84080ed5beceb48c
Jan-Niklas Bohnensack <jan-niklas.bohnensack@rub.de>
2026-05-16 01:23:45 +0200

    bugfix

--- conda-bld/build.sh
+++ conda-bld/build.sh
@@ -3,6 +3,13 @@ set -ex
 PYTHON_INCLUDE_DIR=$(${PYTHON} -c "import sysconfig; print(sysconfig.get_paths()['include'])")
 NUMPY_INCLUDE_DIR=$(${PYTHON} -c "import numpy; print(numpy.get_include())")
 
+if [ -z $IS_MACOS ]; then
+	CXXSTANDARD=17
+	CXXFLAGS="-std=c++17 -stdlib=libc++"
+else
+	CXXSTANDARD=11
+fi
+
 cd $SRC_DIR/crpropa
 mkdir build && cd build
 cmake .. -G Ninja \
@@ -12,6 +19,8 @@ cmake .. -G Ninja \
 	-DPython_INCLUDE_DIR="${PYTHON_INCLUDE_DIR}" \
 	-DPython_INSTALL_PACKAGE_DIR="${SP_DIR}" \
 	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+	-DCMAKE_CXX_STANDARD="${CXXSTANDARD}" \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
 	-DBUILD_DOC=OFF \
 	-DDOWNLOAD_DATA=ON \
 	-DENABLE_COVERAGE=OFF \
