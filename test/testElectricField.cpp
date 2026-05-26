540419fb54c828a99629c112b25bf5fd450e28aa
Jan-Niklas Bohnensack <jan-niklas.bohnensack@rub.de>
2026-05-26 20:22:39 +0200

    added test for ElectricFieldList

--- test/testElectricField.cpp
+++ test/testElectricField.cpp
@@ -6,13 +6,24 @@
 
 using namespace crpropa;
 
-// simple test for uniform electric fields copied from magnetic fields
 TEST(testUniformElectricField, SimpleTest) {
 	UniformElectricField E(Vector3d(-1, 5, 3));
 	Vector3d e = E.getField(Vector3d(1, 0, 0));
 	EXPECT_DOUBLE_EQ(e.getX(), -1);
 	EXPECT_DOUBLE_EQ(e.getY(), 5);
 	EXPECT_DOUBLE_EQ(e.getZ(), 3);
+}
+
+TEST(testElectricFieldList, SimpleTest) {
+	ElectricFieldList E;
+	// Test a list of three electric fields
+	E.addField(new UniformElectricField(Vector3d(1, 0, 0)));
+	E.addField(new UniformElectricField(Vector3d(0, 2, 0)));
+	E.addField(new UniformElectricField(Vector3d(0, 0, 3)));
+	Vector3d e = E.getField(Vector3d(0.));
+	EXPECT_DOUBLE_EQ(e.x, 1);
+	EXPECT_DOUBLE_EQ(e.y, 2);
+	EXPECT_DOUBLE_EQ(e.z, 3);
 }
 
 int main(int argc, char **argv) {
