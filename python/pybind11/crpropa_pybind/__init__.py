9dd28a18f8f141ffc255cddb4c1f8a963141dc81
Jan-Niklas Bohnensack <jan-niklas.bohnensack@rub.de>
2026-08-27 19:16:07 +0200

    reversed accidentally commited changes

--- python/pybind11/crpropa_pybind/__init__.py
+++
@@ -1,1 +0,0 @@
-from .crpropa.crpropa import *
--- python/pybind11/getCRPropaDefines.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cat @CRPropa_BINARY_DIR@/compile_commands.json \
-	| grep -e command -m 1 \
-	| sed -E "s/ /\n/g" \
-	| grep -e "-DCRPROPA" -e "-DFAST_WAVES" -e "-DHAVE_SIMD" \
-	| sed -E "s/\"//g" \
-	| sed -E 's/\\//g' \
